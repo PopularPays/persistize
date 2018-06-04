3ed9937b3cb8049d1a1a4ea2e94625853d0534b2
Matthew Weitzel <mweitzel@gmail.com>
2018-06-04 16:18:19 -0600

    remove trailing whitespace

--- test/models/person.rb
+++ test/models/person.rb
@@ -1,25 +1,25 @@
 class Person < ActiveRecord::Base
-  
+
   has_many :projects
   has_many :tasks, :through => :projects
   has_one :address
-  
+
   def full_name
     "#{first_name} #{last_name}"
   end
-  
+
   def initials
     "#{first_name.first}#{last_name.first}".upcase
   end
-  
+
   def info
     "#{full_name} has #{tasks.count} tasks in #{projects.count} projects"
   end
-  
+
   def city_sentence
     address ? "#{first_name} lives in #{address.city}" : "#{first_name} hasn't told us where he lives"
   end
-  
+
   persistize :full_name, :initials
   persistize :info, :depending_on => [:projects, :tasks]
   persistize :city_sentence, :depending_on => :address
--- test/models/project.rb
+++ test/models/project.rb
@@ -1,10 +1,10 @@
 class Project < ActiveRecord::Base
   has_many :tasks
   belongs_to :person
-  
+
   def completed?
     tasks.any? && tasks.all?(&:completed?)
   end
-  
+
   persistize :completed?, :depending_on => :tasks
 end
